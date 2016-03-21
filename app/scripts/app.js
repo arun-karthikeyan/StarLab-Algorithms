7558317f287b9f0526bc37f7ba702dd790b841dc
mecodesta <the3drevolution@gmail.com>
2016-03-21 13:01:33 -0700

    initial preview static

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+$(document).ready(function(){
+  var transitionTime = 1000;
+  var maxIterations = 10;
+  var text = ['Setting up initial model parameters...', 'Starting Model Training...'];
+  var iterConst = 'iteration ';
+  for(var i=1;i<=maxIterations;++i){
+    text.push(iterConst+i+'...');
+  }
+  text.push('Training Complete !');
+  text.push('Evaluating Test Data...');
+  text.push('Here are your Test Results');
+
+  function transitionText(index){
+
+    //load inner html
+    $('#transitText').html(text[index]);
+    //fade in
+    $('#transitText').fadeTo(transitionTime,1, function(){
+      if(index<(text.length-1)){
+        $('#transitText').fadeTo(transitionTime,0,function(){
+          if((++index)<text.length){
+            transitionText(index);
+          }
+        });
+      }else{
+        $('#test-results').fadeTo(transitionTime,1);
+      }
+    });
+  }
+  transitionText(0);
+});
